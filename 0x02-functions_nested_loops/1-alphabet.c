4268bb64beacd8ff8501915ddc1c3cb4f0475eac
Bitanya Gezahegn <bitanyagidey88@gmail.com>
2022-03-20 13:11:30 -0700

    1.5

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,19 @@
-/*print_alphabet-prints the alphabet in lower case, followed by a new line*/
-#include "main.h"
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ */
 void print_alphabet(void)
 {
-	char letter;
-	for (letter = 'a'; letter<='z';letter++)
-		_putchar(letter);
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
 	_putchar('\n');
 }
