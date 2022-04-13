f5b0ddb0f67d52f6e7cb00151fc9fd9376af047b
Bitanya Gezahegn <bitanyagidey88@gmail.com>
2022-04-13 12:58:12 -0700

    fifth on second

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,29 +1,28 @@
 #include "function_pointers.h"
-#include <stdlib.h>
-
 /**
- * int_index - searches for an integer
- * @array: integer array
- * @size: size of array (number of integers)
- * @cmp: function pointer
- * Return: void
+ * int_index - Search for an integer
+ * @array:int
+ * @size:int
+ * @cmp:function
+ * Return:index of the integer searched for
  */
-
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i, result;
-
-	if (array == NULL || cmp == NULL)
-		return (-1);
+	int i, x;
 
 	if (size <= 0)
-		return (-1);
-
-	for (i = 0; i < size; i++)
 	{
-		result = cmp(array[i]);
-		if (result == 1)
-			return (i);
+		return (-1);
 	}
+		if (size && cmp != NULL && array != NULL)
+		{
+			for (i = 0; i < size; i++)
+			{
+				x = cmp(array[i]);
+				if (x != 0)
+					return (i);
+			}
+		}
 	return (-1);
+
 }
